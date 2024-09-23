7e93957a9982fcfc5325801b7dd4431dcdbffc09
Kirill <kirillsavorin@gmail.com>
2024-09-23 20:01:09 +0200

    class function is changed

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const button = document.querySelector('#addBtn');
 const taskWindow = document.querySelector("#taskWindow");
 const listText = document.querySelector("#list");
 const countTask = document.querySelector("#count");
+const infoText = document.querySelector('#infoText');
 const listArray = [];
 //Add Function
 
@@ -13,9 +14,12 @@ function removeElement(){
 }
 
 function addToList(){
+
+    inputText = taskWindow.value;
     //Check if window its not ""
-    if(taskWindow.value !== ""){
-        const input = taskWindow.value;
+    if(inputText !== ""){
+        const inputText = taskWindow.value;
+        infoText.innerText = ""
 
         //const listItem = document.querySelector('ul');
         // Adding new html element in ul
@@ -23,21 +27,25 @@ function addToList(){
         listText.appendChild(item);
 
         const itemLabel = document.createElement('span');
-        itemLabel.innerText = input;
+        itemLabel.innerText = inputText;
         item.appendChild(itemLabel);
 
         // Event listener for span
         itemLabel.addEventListener("click",
             function(){
-                item.appendChild(itemLabel).classList.toggle("completed");
-                listCompleted = document.querySelectorAll(".completed");
-                countTask.innerText = listCompleted.length 
+                if(item.getAttribute("class") == "completed"){
+                item.setAttribute("class", "");
+                } else{
+                    item.setAttribute("class", "completed");
+                }
             }, false);
 
         // Input cleaned
         taskWindow.value = "";
+        listCompleted = document.querySelectorAll(".completed");
+        countTask.innerText = listCompleted.length;
     }else{
-        alert("You have to write something!");
+        infoText.innerText = "Text is empty!"
     }
 }
 countTask.innerText = 0;
